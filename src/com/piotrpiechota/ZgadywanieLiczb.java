1269655be9847a6636766ae7d3a6abc19d119653
Piotr Piechota <piotr_piechota@poczta.onet.pl>
2019-11-29 16:47:00 +0100

    Proba commitu z IntelliJ

---
+++ src/com/piotrpiechota/ZgadywanieLiczb.java
@@ -0,0 +1,40 @@
+package com.piotrpiechota;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class ZgadywanieLiczb {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Random rand = new Random();
+        int toGuess = rand.nextInt(100) + 1;
+
+        System.out.println(toGuess);
+
+        int guess = getNumber(scanner);
+
+        while (guess != toGuess) {
+
+            if (guess < toGuess) {
+                System.out.println("Za mało!");
+                guess = getNumber(scanner);
+            } else {
+                System.out.println("Za dużo!");
+                guess = getNumber(scanner);
+            }
+        }
+        System.out.println("Zgadłeś!");
+
+        scanner.close();
+    }
+
+    private static int getNumber(Scanner scanner) {
+        System.out.print("Zgadnij liczbę: ");
+        while (!scanner.hasNextInt()) {
+            scanner.next();
+            System.out.print("To nie jest liczba.\nZgadnij liczbę: ");
+        }
+        return scanner.nextInt();
+    }
+}
